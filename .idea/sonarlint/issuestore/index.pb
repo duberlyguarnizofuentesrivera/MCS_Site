e95e5847ee5dff31a82aed3612571db0e440c6d8
Duberly Guarnizo <duberlygfr@gmail.com>
2023-01-27 00:05:28 -0500

    Added "IT Free check" function. Added some more texts. Fixed some CSS,JS code marked by linter.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+F
+portfolio-details.html,9\1\918b23ad75b65c88c48c2bcee574ce4ec5491eb8
+?
+inner-page.html,d\1\d146d866dda18897e4fe6f4016fb87a126d7ff6f
--- assets/js/main.js
+++ assets/js/main.js
@@ -43,36 +43,36 @@
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
+  let navbarlinks = select('#navbar .scrollTo', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
       }
     })
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollTo = (el) => {
+        let header = select('#header')
+        let offset = header.offsetHeight
 
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+        })
+    }
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
@@ -125,21 +125,21 @@
     }
   }, true)
 
-  /**
-   * Scroll with offset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+    /**
+     * Scroll with offset on links with a class name .scrollTo
+     */
+    on('click', '.scrollTo', function (e) {
+        if (select(this.hash)) {
+            e.preventDefault()
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
+            let navbar = select('#navbar')
+            if (navbar.classList.contains('navbar-mobile')) {
+                navbar.classList.remove('navbar-mobile')
+                let navbarToggle = select('.mobile-nav-toggle')
+                navbarToggle.classList.toggle('bi-list')
+                navbarToggle.classList.toggle('bi-x')
+            }
+            scrollTo(this.hash)
     }
   }, true)
 
@@ -149,7 +149,7 @@
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
-        scrollto(window.location.hash)
+          scrollTo(window.location.hash)
       }
     }
   });
